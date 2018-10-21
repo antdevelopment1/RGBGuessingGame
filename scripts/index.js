a27d4bd9b02437d07cf6d695ee0f089439c3e6c7
April Copes <antdevelopment1@gmail.com>
2018-10-20 23:28:08 -0400

    Adds logic for correct guess

--- scripts/index.js
+++ scripts/index.js
@@ -16,5 +16,19 @@ colorDisplay.textContent = pickedColor;
 
 // style.backgroundColor doesnot work in Firefox while style.background works in all browsers
 for (let i = 0; i < squares.length; i++) {
+    // Add initial colors to squares
     squares[i].style.backgroundColor = colors[i];
+    // Add click listeners to squares
+    squares[i].addEventListener('click', function () {
+        // Compare color to picked color
+        let clickedColor = this.style.backgroundColor;
+        if(clickedColor === pickedColor) {
+            console.log('Correct.')
+        } else {
+            console.log(clickedColor);
+            console.log(pickedColor);
+            console.log('Wrong');
+        }
+    })
 }
+
