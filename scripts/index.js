00b96985dcb67677095df7e09afa576b66175a68
April Copes <antdevelopment1@gmail.com>
2018-10-20 23:01:36 -0400

    Adds span tag to h1 element

--- scripts/index.js
+++ scripts/index.js
@@ -4,10 +4,15 @@ let colors = [
     "rgb(0, 255, 0)",
     "rgb(0, 255, 255)",
     "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)",
+    "rgb(255, 0, 255)"
 ];
 
 let squares = document.querySelectorAll('.square');
+let pickedColor = colors[3];
+let colorDisplay = document.querySelector('#colorDisplay');
+colorDisplay.textContent = pickedColor;
+
+
 
 // style.backgroundColor doesnot work in Firefox while style.background works in all browsers
 for (let i = 0; i < squares.length; i++) {
