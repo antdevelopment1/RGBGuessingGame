b8d258c971bf64d290abb1e3b3a042005f46a3d0
April Copes <antdevelopment1@gmail.com>
2018-10-20 22:46:39 -0400

    Creates colors array of rgb values and adds for loop to iterate through each square

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,15 @@
-alert("Connected");
+let colors = [
+    "rgb(255, 0, 0)",
+    "rgb(255, 255, 0)",
+    "rgb(0, 255, 0)",
+    "rgb(0, 255, 255)",
+    "rgb(0, 0, 255)",
+    "rgb(255, 0, 255)",
+];
+
+let squares = document.querySelectorAll('.square');
+
+// style.backgroundColor doesnot work in Firefox while style.background works in all browsers
+for (let i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = colors[i];
+}
