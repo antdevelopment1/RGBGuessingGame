ac97f6a7bda67345395500dd5e6aabcef9d4657a
April Copes <antdevelopment1@gmail.com>
2018-10-27 23:51:53 -0400

    stores  numSquares and generated random color based difficulty level chose

--- scripts/index.js
+++ scripts/index.js
@@ -6,22 +6,42 @@ let resetButton = document.querySelector('#reset');
 let easyBtn = document.querySelector('#easyBtn');
 let hardBtn = document.querySelector('#hardBtn');
 let h1 = document.querySelector('h1');
-let colors = generateRandomColors(6);
+let numSquares = 6;
+let colors = generateRandomColors(numSquares);
 
 easyBtn.addEventListener('click', function() {
     hardBtn.classList.remove('selected');
     easyBtn.classList.add('selected');
+    numSquares = 3;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for (let i = 0; i < squares.length; i++) {
+        if (colors[i]) {
+        squares[i].style.backgroundColor = colors[i];
+        } else {
+            squares[i].style.display = 'none';
+        }
+    }
 })
 
 hardBtn.addEventListener('click', function() {
     hardBtn.classList.add('selected');
     easyBtn.classList.remove('selected');
-})
+    numSquares = 6;
+    colors = generateRandomColors( numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = colors[i];
+        squares[i].style.display = 'block';
+    }
+});
 
 resetButton.addEventListener('click', function() {
 
     // generate all new colors
-    colors = generateRandomColors(6);
+    colors = generateRandomColors(numSquares);
 
     // Pick new random number from array
     pickedColor = pickColor();
