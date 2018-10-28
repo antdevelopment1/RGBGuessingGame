a9ef05c93d0046cedce16cfab2b78703e467ce1a
April Copes <antdevelopment1@gmail.com>
2018-10-27 23:12:49 -0400

    Adds event listeners for easy and hard button

--- scripts/index.js
+++ scripts/index.js
@@ -3,8 +3,20 @@ let squares = document.querySelectorAll('.square');
 let colorDisplay = document.querySelector('#colorDisplay');
 let messageDisplay = document.querySelector('#message');
 let resetButton = document.querySelector('#reset');
+let easyBtn = document.querySelector('#easyBtn');
+let hardBtn = document.querySelector('#hardBtn');
 let h1 = document.querySelector('h1');
 let colors = generateRandomColors(6);
+
+easyBtn.addEventListener('click', function() {
+    hardBtn.classList.remove('selected');
+    easyBtn.classList.add('selected');
+})
+
+hardBtn.addEventListener('click', function() {
+    hardBtn.classList.add('selected');
+    easyBtn.classList.remove('selected');
+})
 
 resetButton.addEventListener('click', function() {
 
